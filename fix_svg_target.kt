fa991ea213f37c5e9a10fc50e988bed3252bd02a
soywiz <soywiz@gmail.com>
2018-10-21 19:56:51 +0200

    Styling SVG navigation

--- fix_svg_target.kt
+++ fix_svg_target.kt
@@ -9,9 +9,15 @@ fun main(args: Array<String>) {
 // draw.io works pretty well, but doesn't set the target="_top" or target="_blank" for links, that is a problem when using <embed src="file.svg" />
 fun updateSvgTarget(file: File) {
     val oldText = file.readText()
-    val newText = oldText.replace(Regex("""<a xlink:href="(.*?)">""")) {
+    val newText = oldText.replace(Regex("""<a\s+(?:target=.*?\s+)?xlink:href="(.*?)">""")) {
         val link = it.groupValues[1]
         """<a target="_top" xlink:href="$link">"""
+    }.let {
+        if (it.contains("<style>")) {
+            it
+        } else {
+            it.replace("<defs>", "<style>a:hover rect { stroke: black; stroke-width: 2; }</style><defs>")
+        }
     }
     file.writeText(newText)
 }
